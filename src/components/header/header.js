0d93469a3ec0c6779afa7ff144f1dc5ea2401e46
William Cunha <williamoliveira2@hotmail.com>
2018-11-10 22:25:33 -0300

    Add Header Container

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Container from '../container';
 import ReaderMessage from '../header-message';
 import './style.css';
 
 const Header = () => (
   <header className="header">
-    <ReaderMessage>Hello World!</ReaderMessage>
+    <Container>
+      <ReaderMessage>Hello World!</ReaderMessage>
+    </Container>
   </header>
 );
 
