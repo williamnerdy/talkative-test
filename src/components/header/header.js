3cea10151306da9d4b16498fb8900392b9da180a
William Cunha <williamoliveira2@hotmail.com>
2018-11-10 21:57:27 -0300

    Test Header

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReaderMessage from '../header-message';
+
+const Header = () => (
+  <header>
+    <ReaderMessage>Hello World!</ReaderMessage>
+  </header>
+);
+
+export default Header;
