d41145e78cdb603a6a43ee85b311b9db56d73b1c
William Cunha <williamoliveira2@hotmail.com>
2018-11-12 21:48:57 -0300

    Add form

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,6 +4,8 @@ import Row from '../row';
 import Col from '../col';
 import Card from '../card';
 import Title from '../title';
+import Form from '../form';
+import Input from '../input';
 
 const Main = () => (
   <Container>
@@ -13,6 +15,20 @@ const Main = () => (
           <Title style={{ textAlign: 'center' }}>
             Sua primeira tarefa é preencher seus dados de acesso:
           </Title>
+          <Form>
+            <Input
+              name="email"
+              type="email"
+              placeholder="Digite seu e-mail"
+              autocorrect="off"
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Digite sua senha"
+              autocomplete="off"
+            />
+          </Form>
         </Card>
       </Col>
       <Col>
