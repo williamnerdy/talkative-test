a68f41db0eff87d8b2793ff6b1bcf11f5c57ab4b
William Cunha <williamoliveira2@hotmail.com>
2018-11-13 00:23:31 -0300

    Prepare API

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://api.dev.talkative.media/v1'
+});
+
+export const login = (email, password) =>
+  api.post('/login/email-and-password/', {
+    email,
+    password
+  });
+
+export const signUp = (name, password, email) =>
+  api.post('/signup/email-and-password/', {
+    name,
+    password,
+    email
+  });
