93710f5128c734b187a3ec21771ba94a785a08af
William Cunha <williamoliveira2@hotmail.com>
2018-11-12 22:45:08 -0300

    Add icon to form button

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,11 +7,12 @@ import Title from '../title';
 import Form from '../form';
 import Input from '../input';
 import Button from '../button';
+import Icon from '../icon';
 
 const Main = () => (
   <Container>
     <Row reverse>
-      <Col>
+      <Col size={7}>
         <Card>
           <Title style={{ textAlign: 'center' }}>
             Sua primeira tarefa é preencher seus dados de acesso:
@@ -30,13 +31,15 @@ const Main = () => (
               autocomplete="off"
             />
             <Row>
-              <Col>
+              <Col size={6}>
                 <Button color="facebook" type="button">
+                  <Icon name="facebook" />
                   <span>Entrar com o Facebook</span>
                 </Button>
               </Col>
-              <Col>
+              <Col size={6}>
                 <Button color="form">
+                  <Icon name="sign-in" />
                   <span>Entrar</span>
                 </Button>
               </Col>
@@ -44,7 +47,7 @@ const Main = () => (
           </Form>
         </Card>
       </Col>
-      <Col>
+      <Col size={5}>
         <p style={{ padding: '30px 0' }}>
           Você foi convidado a participar da mais inovadora ferramenta de mídia
           para micro-influenciadores nas redes sociais.
