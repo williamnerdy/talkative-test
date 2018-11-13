d34d7d56adee7b79effd23d3bc5c4b5b7343b031
William Cunha <williamoliveira2@hotmail.com>
2018-11-12 23:22:31 -0300

    Add links to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,19 +4,24 @@ import Row from '../row';
 import Col from '../col';
 import Logo from '../logo';
 import Copyright from '../copyright';
+import Link from '../link';
 import './style.css';
 
 const Footer = () => (
   <footer className="footer">
     <Container>
       <Row>
-        <Col size={4}>
+        <Col size={3}>
           <Logo />
         </Col>
         <Col size={4}>
           <Copyright />
         </Col>
-        <Col size={4}>Footer center</Col>
+        <Col size={5}>
+          <Link to="//example.com">Enviar Feedback</Link>
+          <Link to="//example.com">Sobre Nós</Link>
+          <Link to="//example.com">Fale Conosco</Link>
+        </Col>
       </Row>
     </Container>
   </footer>
