fa7607cc4b9659909eaa7f6bf9d8dabb250cdb60
William Cunha <williamoliveira2@hotmail.com>
2018-11-12 23:55:49 -0300

    Remove main and add sign-up

---
+++ src/screens/sign-up/sign-up.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Container from '../../components/container';
+import Row from '../../components/row';
+import Col from '../../components/col';
+import Card from '../../components/card';
+import Title from '../../components/title';
+import Form from '../../components/form';
+import Input from '../../components/input';
+import Button from '../../components/button';
+import Icon from '../../components/icon';
+import Countdown from '../../components/countdown';
+
+const SignUp = () => (
+  <Container>
+    <Row reverse>
+      <Col size={7}>
+        <Card>
+          <Title style={{ textAlign: 'center' }}>
+            Para validar seu convite é preciso cadastrar os seguintes dados:
+          </Title>
+          <Form>
+            <Input
+              name="email"
+              type="email"
+              placeholder="Digite seu e-mail"
+              autoCorrect="off"
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Escolha sua senha"
+              autoComplete="off"
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Confirme sua senha"
+              autoComplete="off"
+            />
+            <Row>
+              <Col size={6}>
+                <Button color="facebook" type="button">
+                  <Icon name="facebook" />
+                  <span>Criar com o Facebook</span>
+                </Button>
+              </Col>
+              <Col size={6}>
+                <Button color="form">
+                  <Icon name="user" />
+                  <span>Criar conta</span>
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+      </Col>
+      <Col size={5}>
+        <div style={{ padding: '30px 0' }}>
+          <div>
+            Você foi convidado a participar da mais inovadora ferramenta de
+            mídia para micro-influenciadores nas redes sociais.
+          </div>
+          <br />
+          <div>
+            <span>Este convite expira em: </span>
+            <Countdown />
+          </div>
+        </div>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default SignUp;
